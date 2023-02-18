14cd048aef867de977eabb8275c4c12b3793bbcf
Ntuthuko <ntuthukolu@gmail.com>
2023-02-17 16:59:33 -0800

    I sometimes suffer from insomnia...

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ * main - Prints alphabet in lower case
  *
  * Return: Always 0 (Success)
  */
@@ -12,7 +12,8 @@ int main(void)
 
 	for (lowCA = 'a'; lowCA <= 'z'; lowCA++)
 		putchar(lowCA);
-		printf("\n");
+	
+	putchar('\n');
 
 	return (0);
 }
