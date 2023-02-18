7fc86ad01da9156524c351760810e5f11654d0d8
Ntuthuko <ntuthukolu@gmail.com>
2023-02-17 17:42:11 -0800

    bug fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,17 @@ int main(void)
 {
 	char lowCA;
 
-	for(lowCA = 'a'; lowCA <= 'z'; lowCA++){
+	for(lowCA = 'a'; lowCA <= 'z'; lowCA++)
 		putchar(lowCA);
 	
 
 	for(lowCA = 'A'; lowCA <= 'Z'; lowCA++)
 		putchar(lowCA);
-	
 
 	putchar('\n');
 
-	
+
 	return(0);
+	
+	
 }
