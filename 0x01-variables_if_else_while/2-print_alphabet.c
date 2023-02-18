8001b1bd4ec8515c323647b84a26e7f7e7026ba4
Ntuthuko <ntuthukolu@gmail.com>
2023-02-17 16:46:05 -0800

    Sometimes I suffer from insomina

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowCA;
+
+	for (lowCA = 'a'; lowCA <= 'z'; lowCA++)
+		putchar(lowCA);
+		printf("\n");
+
+	return (0);
+}
