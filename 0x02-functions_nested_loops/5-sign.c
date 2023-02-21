251a31162d5ff9a21d76780e6c53891043eefae9
Ntuthuko <ntuthukolu@gmail.com>
2023-02-21 15:30:07 -0800

    Absolute value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
  * print_sign - function that prints sign of a number
  * @n: number of of sign that will be printed.
  * Return: 1 if greater than zero
- * 	   0 if equal to zero
- * 	   -1 if less than zero
+ * 0 if equal to zero
+ * -1 if less than zero
  */
 int print_sign(int n)
 {
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Function comuputes absolute value of int
+ * @n: The int
+ * Return: Computed value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
