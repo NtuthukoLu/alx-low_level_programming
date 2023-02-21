eea9f7d13157fe4c90dfac0aa7e6de7484986592
Ntuthuko <ntuthukolu@gmail.com>
2023-02-21 15:59:04 -0800

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that adds two arguments
+ * @a: 1st interger
+ * @b: 2nd interger
+ * Return: The sum
+ */
+int add(int a, int b)
+{
+	int sum = a+b;
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 /**
  * times_table - Funtion for 9 times table initial value 0
  */
-void times table(void)
+void times_table(void)
 {
 	int num, mult, prod;
 
