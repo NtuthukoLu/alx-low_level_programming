b740ed52352f36236a278988a7c63465b5ed02cd
Ntuthuko <ntuthukolu@gmail.com>
2023-02-23 09:50:54 -0800

    2 largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,17 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && b > c)
+    if (a >= b && b >= c)
     {
         largest = a;
     }
-    else if (b > a && a > c)
+    else if (b >= a && a >= c)
     {
         largest = b;
     }
-    else if (c > b && b > c)
+    else
     {
         largest = c;
-    }
-    else 
-    {
-	    printf("No large number");
     }
 
     return (largest);
