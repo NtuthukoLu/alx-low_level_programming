afca432db2e2bf2f933674f4e531b3140cdf9851
Ntuthuko <ntuthukolu@gmail.com>
2023-02-27 13:48:34 -0800

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @s: pointer of type char
+ */
+int _strlen(char *s);
+{
+	int length = 0;
+
+	while (string[length] != '\0') length++;
+	return length;
+}
