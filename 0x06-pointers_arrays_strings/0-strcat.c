698bf50bb8688b63d08b57da525efef7a1b90640
Ntuthuko <ntuthukolu@gmail.com>
2023-03-03 02:41:58 +0200

    Concatinating

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,10 @@ char *_strcat(char *dest, char *src)
 {
 	int dest_length = 0, int src_length = 0;
 
-	while (dest[dest_length] != '\o')
+	while (dest[dest_length] != '\0')
 		dest_length++;
 
-	while (src[src_length] != '\o')
+	while (src[src_length] != '\0')
 	{
 		dest[dest_length] = src[src_length];
 		dest_length++;
