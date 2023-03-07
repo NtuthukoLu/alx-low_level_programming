3cd94361b6cfa2a181f54f670c8b088486dbad75
Ntuthuko <ntuthukolu@gmail.com>
2023-03-06 18:22:01 -0800

    more arrays

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,27 +8,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
 	unsigned int bytes = 0;
+	int index;
 
 	while (*s)
 	{
-		while (accept[i])
+		for (index = 0; accept[index]; index++)
 		{
-			if (*s == accept[i])
+			if (*s == accept[index])
 			{
 				bytes++;
 				break;
 			}
-			else if (accept[i + 1] == '\0')
+
+			else if (accept[index + 1] == '\0')
 				return (bytes);
 		}
 
 		s++;
-
 	}
 
 	return (bytes);
 }
-
-
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any set of bytes.
+ * @s: string to be searched
+ * @accept: the number of bytes
+ *
+ * Return: Pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+
+		s++;
+	}
+
+	return ('\0');
+}
+}
