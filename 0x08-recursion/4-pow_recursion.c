674fbb98768e76e3532a4c0c84338a237e39d513
Ntuthuko <ntuthukolu@gmail.com>
2023-03-08 19:54:02 +0200

    done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,16 +4,16 @@
  * @x: the base integer
  * @y: the integer to raise with
  */
-int _pow_recursion(int x , int y)
+int _pow_recursion(int x, int y)
 {
-	if ( y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
-	else if ( y == 0)
+	else if (y == 0)
 	{
 		return (1);
 	}
 	else
-		return (x * _pow_recursion(x , y - 1));
+		return (x * _pow_recursion(x, y - 1));
 }
