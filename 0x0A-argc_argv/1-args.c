2e0abd0141a1a800c80eaa740172f50710eb6356
Ntuthuko <ntuthukolu@gmail.com>
2023-03-16 03:09:29 +0200

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments
+ * @argc: number of arguments
+ * @argv: pointer to argv
+ * Return: Always 0
+ */
+int main (int argc ,__attribute__((unused)) char *agrv[])
+{
+	printf("%d\n",argc);
+
+	return (0);
+}
