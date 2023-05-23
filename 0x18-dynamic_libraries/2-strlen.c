3dc1c729d63353465740a3af68e7d742db218124
Ntuthuko <ntuthukolu@gmail.com>
2023-05-22 17:02:31 -0700

    task 0

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @s: pointer of type char
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
