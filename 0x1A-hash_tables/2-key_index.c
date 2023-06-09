38c3d33cf99e903aee09ea06dbf3c3d271deac65
Ntuthuko <ntuthukolu@gmail.com>
2023-06-09 16:03:01 +0200

    index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * key_index - gets the index of given key for hashtable
+ * @key: str containing key
+ * @size: size of the hash table's array
+ *
+ * Return: index of given key in hash_table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
