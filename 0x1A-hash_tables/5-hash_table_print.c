4132ecac08f2e4e51285e3ca9fd2d62760baf217
Ntuthuko <ntuthukolu@gmail.com>
2023-06-11 15:12:31 -0700

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, count = 0, j = 0;
+	hash_node_t *curr;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			curr = ht->array[i];
+			while (curr)
+			{
+				count++;
+				curr = curr->next;
+			}
+		}
+	}
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			curr = ht->array[i];
+			while (curr)
+			{
+				printf("'%s': '%s'", curr->key, curr->value);
+				j++;
+				if (j < count)
+					printf(", ");
+				curr = curr->next;
+			}
+		}
+	}
+	printf("}\n");
+}
